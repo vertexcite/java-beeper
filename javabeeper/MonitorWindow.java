aea8927f9cf5ac9d8c55eef4a12cd042e4481e46
Randall Britten <3136161+vertexcite@users.noreply.github.com>
2012-10-21 00:11:09 +1300

    Changed "mute" toggle button back to checkbox called "sound enabled".
    Probably need to include NetBeans .form files for GUI designer.

--- javabeeper/MonitorWindow.java
+++ javabeeper/MonitorWindow.java
@@ -32,9 +32,8 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
         resetSnoozeDelayTime = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         timeRemaining = new javax.swing.JTextField();
-        mute = new javax.swing.JToggleButton();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
+        soundEnabled = new javax.swing.JCheckBox();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Java Beeper");
@@ -59,16 +58,15 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
             }
         });
 
-        mute.setText("Mute");
-        mute.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                muteActionPerformed(evt);
-            }
-        });
-
         jLabel3.setText("Version: 0.17");
 
-        jLabel4.setText("(Note: resetting snooze will unmute again)");
+        soundEnabled.setSelected(true);
+        soundEnabled.setText("Enable audio. (Note: if disabled, then resetting snooze will enable audio again)");
+        soundEnabled.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                soundEnabledActionPerformed(evt);
+            }
+        });
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -77,24 +75,22 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
             .add(layout.createSequentialGroup()
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(layout.createSequentialGroup()
-                        .add(23, 23, 23)
-                        .add(mute)
-                        .add(18, 18, 18)
-                        .add(jLabel4))
-                    .add(layout.createSequentialGroup()
                         .addContainerGap()
                         .add(jLabel3))
-                    .add(layout.createSequentialGroup()
-                        .add(20, 20, 20)
-                        .add(jLabel1)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
-                        .add(resetSnoozeDelayTime, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 100, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                     .add(layout.createSequentialGroup()
                         .add(130, 130, 130)
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                             .add(jLabel2)
-                            .add(timeRemaining, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 390, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(109, Short.MAX_VALUE))
+                            .add(timeRemaining, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 390, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
+                    .add(layout.createSequentialGroup()
+                        .add(20, 20, 20)
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(soundEnabled)
+                            .add(layout.createSequentialGroup()
+                                .add(jLabel1)
+                                .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
+                                .add(resetSnoozeDelayTime, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 100, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))))
+                .addContainerGap(56, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -103,25 +99,19 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
                 .add(jLabel2)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(timeRemaining, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 57, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 62, Short.MAX_VALUE)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(jLabel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .add(resetSnoozeDelayTime, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(mute)
-                    .add(jLabel4))
-                .add(33, 33, 33)
+                .add(soundEnabled)
+                .add(34, 34, 34)
                 .add(jLabel3)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void muteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_muteActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_muteActionPerformed
 
     private void resetSnoozeDelayTimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetSnoozeDelayTimeActionPerformed
 
@@ -132,14 +122,17 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
         // TODO add your handling code here:
     }//GEN-LAST:event_timeRemainingActionPerformed
 
+    private void soundEnabledActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_soundEnabledActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_soundEnabledActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JToggleButton mute;
     private javax.swing.JTextField resetSnoozeDelayTime;
+    private javax.swing.JCheckBox soundEnabled;
     private javax.swing.JTextField timeRemaining;
     // End of variables declaration//GEN-END:variables
 
@@ -149,7 +142,7 @@ public class MonitorWindow extends javax.swing.JFrame implements SnoozeObserver 
     
     @Override
     public void setSoundEnabled(boolean soundEnabled) {
-        this.mute.setSelected(!soundEnabled);		
+        this.soundEnabled.setSelected(soundEnabled);		
     }
 
     @Override
